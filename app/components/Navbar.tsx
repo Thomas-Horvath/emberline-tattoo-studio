6273565b8866c3dfd11915516f2684deb4e16ca0
Thomas-Horvath <thomas.horvath0416@gmail.com>
2026-02-23 19:00:51 +0100

    Footer, update Navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,27 +1,38 @@
+'use client';
+
 import Image from "next/image";
 import Link from "next/link";
+import navItems from "@/Data/Navlinks.js";
 
-const navItems = [
-    { href: "/#munkaim", label: "Munkáim" },
-    { href: "/#stilusok", label: "Stílusok" },
-    { href: "/#folyamat", label: "Folyamat" },
-    { href: "/#velemenyek", label: "Vélemények" },
-];
+import { useEffect, useState } from "react";
+
 
 export default function Navbar() {
+    const [scrolled, setScrolled] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            setScrolled(window.scrollY > 40);
+        };
+
+        window.addEventListener("scroll", handleScroll);
+        return () => window.removeEventListener("scroll", handleScroll);
+    }, []);
+
     return (
         <header className="fixed top-0 left-0 z-50 w-full">
 
             <div
-                className="h-16 mt-4 w-full
-                           bg-zinc-950/650 border-y border-zinc-800/80 shadow-[0_0_0_1px_rgba(0,0,0,0.2)]
-                           backdrop-blur-md"
+                className={`h-16 w-full  transition-all duration-300
+                   ${scrolled ? "h-14 mt-0 border-b backdrop-blur-3xl" : "h-16 mt-4 border-y backdrop-blur-md"}
+                           bg-zinc-950/650  border-zinc-800/80 shadow-[0_0_0_1px_rgba(0,0,0,0.2)]
+                           `}
             >
-                <div className="flex items-center justify-between max-w-[1400px] mx-auto h-full">
+                <div className="flex items-center justify-between max-w-350 mx-auto h-full">
 
                     {/* BAL: LOGÓ + BRAND */}
                     <Link href="/" className="flex items-center gap-2 px-4">
-                        {/* Itt később cseréld a logódra (SVG/PNG) */}
+                       
                         <Image src='/logo1.png' alt="Logo" width={80} height={80} className="h-14 w-14" />
 
                         <div className="leading-none">
@@ -37,7 +48,7 @@ export default function Navbar() {
                     {/* KÖZÉP: NAV */}
                     <nav className="hidden md:flex items-center gap-10 text-xs tracking-[0.22em] text-zinc-300">
                         {navItems.map((item) => (
-                            <a
+                            <Link
                                 key={item.href}
                                 href={item.href}
                                 className="after:absolute after:left-0 relative
@@ -45,17 +56,18 @@ export default function Navbar() {
                                            uppercase
                                            after:bg-orange-500/90
                                            after:transition-all transition
-                                           after:-bottom-2 hover:text-zinc-100"
+                                           after:-bottom-2 
+                                           hover:text-orange-400"
                             >
                                 {item.label}
-                            </a>
+                            </Link>
                         ))}
                     </nav>
 
                     {/* JOBB: CTA */}
                     <div className="pr-4">
-                        <a
-                            href="#idopont"
+                        <Link
+                            href="/kapcsolat"
                             className="inline-flex items-center justify-center
                                        h-10 px-5 rounded-lg
                                        font-semibold text-xs tracking-[0.18em] uppercase
@@ -64,7 +76,7 @@ export default function Navbar() {
                                        text-zinc-950"
                         >
                             Időpontfoglalás
-                        </a>
+                        </Link>
                     </div>
                 </div>
             </div>
