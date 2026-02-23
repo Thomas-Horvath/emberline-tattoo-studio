f0d8e5a4e5469f3258658034b2cf8117dd916547
Thomas-Horvath <thomas.horvath0416@gmail.com>
2026-02-23 19:02:35 +0100

    layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -40,7 +41,9 @@ export default function RootLayout({
             {children}
           </div>
 
+          
         </div>
+        <Footer />
       </body>
     </html>
   );
